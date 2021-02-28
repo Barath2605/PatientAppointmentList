d5452a170e1d3f7867f26a52767cf26d0886ab91
Barath2605 <55592058+Barath2605@users.noreply.github.com>
2021-02-28 16:42:05 +0530

    Initial commit

---
+++ login/adminLogin/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdminpageComponent } from './adminpage/adminpage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminpageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ user-angular-frontend/src/app/todo.ts
@@ -0,0 +1,7 @@
+export class Todo {
+  id: string;
+  title: string;
+  titledr: string;
+  completed: boolean;
+  createdAt: Date;
+}
